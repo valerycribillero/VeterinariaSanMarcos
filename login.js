8361ef69953a7494483ead7376b29dc6fbf54ef1
Karla !! <ka.ferrada@duocuc.cl>
2026-09-04 21:39:36 -0400

    añadi la pantalla de servicios y trabajo en la de consulta

---
+++ login.js
@@ -0,0 +1,25 @@
+function ingresar(){
+    let correo = document.getElementById("correo").value;
+    let clave = document.getElementById("clave").value;
+
+    if (correo === "" || clave === ""){
+        alert("Debe completar todos los campos")
+        return;
+    }
+
+    let formatoCorreo = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (clave.length !== 4){
+        alert("La clave debe ser de 4 caracteres");
+        return;
+    }
+        //entrada usuario
+    if (correo === "dueño@veterinaria.cl" && clave === "1234"){
+        window.location.href = "veterinaria.html"; // se debe hacer una pantalla de usuario
+    }
+    // else if (correo === "usuario@demo.cl" && clave === "5678") //debemos hacer cuentas especificas para los distintos usuarios
+    //     window.location.href = "usuario.html";
+    else{
+        alert("Hubo un error al intentar ingresar.");
+    }
+}
